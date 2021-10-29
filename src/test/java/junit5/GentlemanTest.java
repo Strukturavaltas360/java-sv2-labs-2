3e8811f809f269527bae9ad0ccaaca331a16cbe0
BenkoAta <benkoata@gmail.com>
2021-10-29 18:22:41 +0200

    JUnit 5 gyakorlati feladat hibajavítás

--- src/test/java/junit5/GentlemanTest.java
+++ src/test/java/junit5/GentlemanTest.java
@@ -1,16 +1,13 @@
 package junit5;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GentlemanTest {
     @Test
     public void testSayHello() {
         Gentleman gentleman = new Gentleman();
-        assertEquals(gentleman.sayHello("John Doe"), "Hello John Doe!");
-        assertEquals(gentleman.sayHello(null), "Hello Anonymous!");
+        assertEquals("Hello John Doe!", gentleman.sayHello("John Doe"));
+        assertEquals("Hello Anonymous!", gentleman.sayHello(null));
     }
 }
