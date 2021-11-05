d32977b83b3d4bf9e188c49519a57717d0e88e82
BenkoAta <benkoata@gmail.com>
2021-11-05 14:38:03 +0100

    MeetingRooms projektfeladat megoldása

---
+++ src/main/java/meetingrooms/MeetingRoom.java
@@ -0,0 +1,46 @@
+package meetingrooms;
+
+public class MeetingRoom {
+    private String name;
+    private int length;
+    private int width;
+
+    public MeetingRoom(String name, int length, int width) {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getArea() {
+        return length * width;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %d * %d = %d", name, width, length, getArea());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
