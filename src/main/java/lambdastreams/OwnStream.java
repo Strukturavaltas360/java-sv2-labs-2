b5f796fce75c1df5225335612d64d22a10e78667
BenkoAta <benkoata@gmail.com>
2022-01-16 15:28:26 +0100

    saját stream test

--- src/main/java/lambdastreams/OwnStream.java
+++ src/main/java/lambdastreams/OwnStream.java
@@ -1,5 +1,6 @@
 package lambdastreams;
 
+import java.util.Comparator;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.Consumer;
@@ -7,22 +8,54 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class OwnStream {
+    private class TestSpliterator extends Spliterators.AbstractSpliterator<Integer> implements Runnable {
+        private int cntr;
+
+        protected TestSpliterator(long est, int additionalCharacteristics) {
+            super(est, additionalCharacteristics);
+        }
+
+        @Override
+        public void run() {
+            System.out.println("onClose");
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super Integer> action) {
+            cntr++;
+            if (cntr < 10) {
+                System.out.println("tryAdvance, cntr:" + cntr);
+                action.accept(cntr);
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super Integer> action) {
+            super.forEachRemaining(action);
+        }
+
+        @Override
+        public long getExactSizeIfKnown() {
+            return super.getExactSizeIfKnown();
+        }
+
+        @Override
+        public boolean hasCharacteristics(int characteristics) {
+            return super.hasCharacteristics(characteristics);
+        }
+
+        @Override
+        public Comparator<? super Integer> getComparator() {
+            return super.getComparator();
+        }
+    }
 
     public Stream<Integer> createStream() {
-        return StreamSupport.stream(
-                new Spliterators.AbstractSpliterator<Integer>(Long.MAX_VALUE, Spliterator.ORDERED) {
-                    private int cntr;
-                    @Override
-                    public boolean tryAdvance(Consumer<? super Integer> action) {
-                        cntr++;
-                        if (cntr < 10) {
-                            System.out.println("tryAdvance, cntr:" + cntr);
-                            action.accept(cntr);
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }
-                }, false);
+        TestSpliterator testSpliterator = new TestSpliterator(Long.MAX_VALUE, Spliterator.ORDERED);
+        Stream<Integer> result = StreamSupport.stream(testSpliterator, false);
+        result.onClose(testSpliterator);
+        return result;
     }
 }
--- src/test/java/lambdastreams/OwnStreamTest.java
+++ src/test/java/lambdastreams/OwnStreamTest.java
@@ -2,22 +2,26 @@ package lambdastreams;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OwnStreamTest {
     OwnStream os = new OwnStream();
+
     @Test
     void createStream() {
-        Stream<Integer> stream = os.createStream();
-        stream.forEach(this::printAndWait);
+        try (Stream<Integer> stream = os.createStream()) {
+            stream.peek(this::printAndWait)
+                    .collect(Collectors.toList());
+        }
     }
 
     private void printAndWait(Integer i) {
         System.out.println("stream: " + i);
         try {
-            Thread.sleep(1000);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
             throw new IllegalStateException("Process has been interrupted", e);
         }
